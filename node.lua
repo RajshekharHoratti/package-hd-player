af8038775ea0a6297dc3182c215cfefcdb1a57f6
rajshekharh@pixzee.in <rajshekharh@pixzee.in>
2016-10-26 15:26:25 +0530

    test 5

--- node.lua
+++ node.lua
@@ -357,7 +357,7 @@ local VideoJob = function(item, ctx, fn)
             if portrait then
                 width, height = height, width
             end
-            local x1, y1, x2, y2 = util.scale_into(NATIVE_WIDTH, NATIVE_HEIGHT, width, height)
+            local x1, y1, x2, y2 = util.scale_into(width, height, width, height)
             res:layer(layer):rotate(rotation):target(x1, y1, x2, y2, ramp(
                 ctx.starts, ctx.ends, now, Config.get_switch_time()
             ))
